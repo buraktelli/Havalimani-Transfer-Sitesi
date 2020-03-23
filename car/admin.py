33a105e1450c3fed673f3bd8f00266c5489531d0
Burak <btelli37@hotmail.com>
2020-03-23 10:14:45 +0300

    9. video yapıldı.

--- car/admin.py
+++ car/admin.py
@@ -12,12 +12,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['status']
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'price', 'status']
+    list_display = ['title', 'category', 'image_tag', 'price', 'status']
+    readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
     inlines = [CarImageInline]
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'car', 'image']
+    list_display = ['title', 'car', 'image_tag']
+    readonly_fields = ('image_tag',)
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Car, CarAdmin)
--- car/models.py
+++ car/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -19,6 +22,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
 
 class Car(models.Model):
     STATUS = (
@@ -42,6 +49,10 @@ class Car(models.Model):
     def __str__(self):
         return self.title
 
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
 class Images(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
@@ -49,3 +60,7 @@ class Images(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
